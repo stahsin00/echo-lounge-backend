4e259aed4eaadb6ffc2cd57f4676c6522583df84
stahsin00 <lamisa1998@hotmail.com>
2024-09-23 22:05:02 -0700

    deploy and debug

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected successfully.');
+        console.log('Connected to MongoDB.');
     } catch (err) {
         console.error('MongoDB connection error:', err);
         process.exit(1);
